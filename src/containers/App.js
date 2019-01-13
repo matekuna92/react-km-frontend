e7e9b482b8ef957bc894f853a1417465a6247758
matekuna92 <kunamate66@gmail.com>
2019-01-13 19:56:33 +0100

    app moved to containers, prop-types installed

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,29 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 //import NameInput from './nameInputFile.jsx';
-import DeviceForm from './components/deviceForm';
-import DeviceList from './components/deviceList';
+import DeviceForm from '../components/deviceForm';
+import DeviceList from '../components/deviceList';
 import { connect } from 'react-redux';
-import { addDevice } from './actions/actionTypes';
+import { addDevice } from '../actions/actionTypes';
 
 class App extends Component {
     constructor()
     {
         super();
 
-        this.state = {
-            devices: [
-                { id: 1, name: 'LG', amount: 2 },
-                { id: 2, name: 'Samsung', amount: 5 },
-                { id: 3, name: 'Sony', amount: 15 },
-                { id: 4, name: 'Huawei', amount: 3 },
-                { id: 5, name: 'SilverCrest', amount: 22 },
-                { id: 6, name: 'Motorola', amount: 8 },
-                { id: 7, name: 'NoName', amount: 11 },
-                { id: 8, name: 'Apple', amount: 6 }
-            ]
-        }
-
         this.handleAddDevice = this.handleAddDevice.bind(this);
     }
 
     handleAddDevice(device)
     {
-        this.setState({
-            devices: [...this.state.devices, 
-            Object.assign(device, { id: this.state.devices[this.state.devices.length -1].id + 1 })]
-        })
+        this.props.onAddDevice(device);
+
+      // props előtti állapot, ahol a devices array még helyben, itt az App-ban volt definiálva a reducer helyett  
+     //   this.setState({
+     //       devices: [...this.state.devices, 
+     //       Object.assign(device, { id: this.state.devices[this.state.devices.length -1].id + 1 })]
+     //  })
     }
 
     render()
@@ -43,7 +33,7 @@ class App extends Component {
             <div style={{ marginBottom: 50 }}> 
 
                 <DeviceForm onAddDevice={ this.handleAddDevice }/>
-                <DeviceList devices={ this.state.devices }/>
+                <DeviceList devices={ this.props.devices }/>
                 
             </div>
         )
--- src/reducers/deviceReducer.js
+++ src/reducers/deviceReducer.js
@@ -1,12 +1,25 @@
 import { ADD_DEVICE } from '../actions/actionTypes';
 
-const deviceReducer = (state = { devices: [] }, action ) => { 
+const initialState = {
+    devices: [
+        { id: 1, name: 'LG', amount: 2 },
+        { id: 2, name: 'Samsung', amount: 5 },
+        { id: 3, name: 'Sony', amount: 15 },
+        { id: 4, name: 'Huawei', amount: 3 },
+        { id: 5, name: 'SilverCrest', amount: 22 },
+        { id: 6, name: 'Motorola', amount: 8 },
+        { id: 7, name: 'NoName', amount: 11 },
+        { id: 8, name: 'Apple', amount: 6 }
+    ]
+}
+
+const deviceReducer = (state = initialState, action ) => { 
     switch(action.type)
     {
         case ADD_DEVICE: return {
             ...state,
             devices: [...state.devices, 
-                Object.assign(action.value, { id: this.state.devices[this.state.devices.length -1].id + 1 })]
+                Object.assign(action.value, { id: state.devices[state.devices.length -1].id + 1 })]
         }           /* Mivel az app.js-ben a function(device) paramétert kapott, így ott a tömbben a device-t adtuk át az Object.assign-nak -->
                      reducer esetén viszont már action-t kap paraméterként, így az átadott objektum az action.value lesz ! */
         default: return state; 
