9c92ebb42acc8feb7d4a88e0ded019c2e8679504
matekuna92 <kunamate66@gmail.com>
2019-01-14 23:07:49 +0100

    localhost:3005 moved to .env, but 404 error returns

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
+//const apiURL = process.env.FRONTEND_URL;
+//console.log('CURRENT URL IS : ', apiURL);
+
 function getDevices()
 {
-    return axios.get('http://localhost:3005/devices');  // return nélkül nincs axiosPromise visszatérési érték, 
+    return axios.get('http://localhost:3005/devices');
+    //return axios.get(`${apiURL}/devices`)  // return nélkül nincs axiosPromise visszatérési érték, 
                                                         // így nincs response, és a .then(..response.data) az App.js-ben undefined error-t dob!
 }
 
+function addDevice(device)
+{
+    return axios.post('http://localhost:3005/devices', device);
+    //return axios.post(`${apiURL}/devices`, device);
+}
+
 export {
-    getDevices
+    getDevices,
+    addDevice
 } 
